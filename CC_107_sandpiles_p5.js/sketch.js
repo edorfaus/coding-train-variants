6c3ea5b49b3f60da7ffe5475f23175aa5c3878b7
Frode Austvik <edorfaus@xepher.net>
2018-06-26 15:13:58 +0200

    CC 107: Added ability to zoom in on the sandpiles.
    This can be turned off/on with a new checkbox.
    
    Also added a descriptive label for the grains slider.

--- CC_107_sandpiles_p5.js/sketch.js
+++ CC_107_sandpiles_p5.js/sketch.js
@@ -18,20 +18,41 @@ let colors = [
 let sandpiles;
 let grainsSlider;
 let grainsSpan;
+let renderer;
 
 function setup() {
 	createCanvas(600, 600);
 	pixelDensity(1);
 
-	grainsSlider = createSlider(0, 100, 1, 1);
-	grainsSpan = createSpan();
+	renderer = renderZoomed;
+	createCheckbox('Zoom', true).changed(function() {
+		renderer = this.checked() ? renderZoomed : renderPixels;
+		background(colors[0][0], colors[0][1], colors[0][2]);
+	});
+
+	let div = createDiv();
+	let label = createElement('label', 'Grains added per frame:');
+	label.parent(createDiv().parent(div));
+	grainsSlider = createSlider(0, 100, 1, 1).parent(div);
+	grainsSpan = createSpan().parent(div);
+	let id = createNewId();
+	grainsSlider.elt.id = id;
+	label.elt.htmlFor = id;
 
 	sandpiles = new ExpandingRectangularSandpileGroup(0);
 
 	background(colors[0][0], colors[0][1], colors[0][2]);
 }
 
-function render() {
+function createNewId() {
+	let id = Math.random().toString(36).slice(2);
+	while (document.getElementById(id)) {
+		id += Math.random().toString(36).slice(2);
+	}
+	return id;
+}
+
+function renderPixels() {
 	loadPixels();
 
 	let midX = Math.floor(width / 2), midY = Math.floor(height / 2);
@@ -57,11 +78,42 @@ function render() {
 	updatePixels();
 }
 
+function renderZoomed() {
+	background(colors[0][0], colors[0][1], colors[0][2]);
+
+	// Calculate size (rounded down to nearest pixel)
+	let cellsX = sandpiles.maxX - sandpiles.minX + 1;
+	let cellsY = sandpiles.maxY - sandpiles.minY + 1;
+	let cellWidth = Math.max(1, Math.floor(width / cellsX));
+	let cellHeight = Math.max(1, Math.floor(height / cellsY));
+
+	// Center it
+	let startX = Math.floor((width - cellWidth * cellsX) / 2);
+	let startY = Math.floor((height - cellHeight * cellsY) / 2);
+
+	// Render it
+	noStroke();
+	let x = startX, y = startY, leftPile = sandpiles.northWest;
+	while (leftPile !== null) {
+		for (let pile = leftPile; pile !== null; pile = pile.east) {
+			let col = colors[pile.grains] || defaultColor;
+
+			fill(col[0], col[1], col[2]);
+			rect(x, y, cellWidth, cellHeight);
+
+			x += cellWidth;
+		}
+		x = startX;
+		y += cellHeight;
+		leftPile = leftPile.south;
+	}
+}
+
 function draw() {
 	let grains = grainsSlider.value();
 	grainsSpan.html(grains);
 
 	sandpiles.addGrains(grains);
 
-	render();
+	renderer();
 }
